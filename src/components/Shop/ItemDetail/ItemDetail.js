2502b18b306ad0a66425cb0122d1649ae129cebb
Emiliano <emilianodebenedetti@outlook.com>
2024-10-14 19:08:28 -0300

    Elimino guía de talles en ItemDetail

--- src/components/Shop/ItemDetail/ItemDetail.js
+++ src/components/Shop/ItemDetail/ItemDetail.js
@@ -36,28 +36,6 @@ export const ItemDetail = ({ product, terminar, onAdd }) => {
                   </button>
                 ))}
             </h3>
-            {/* Boton para modal*/}
-            <label 
-              htmlFor="my_modal_6"
-              className='btn bg-transparent border-none hover:bg-transparent'
-            >
-              Guia de talles <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 256 256"><g fill="currentColor"><path d="M229.66 90.34L90.34 229.66a8 8 0 0 1-11.31 0L26.34 177a8 8 0 0 1 0-11.31L165.66 26.34a8 8 0 0 1 11.31 0L229.66 79a8 8 0 0 1 0 11.34Z" opacity=".2"/><path d="m235.32 73.37l-52.69-52.68a16 16 0 0 0-22.63 0L20.68 160a16 16 0 0 0 0 22.63l52.69 52.68a16 16 0 0 0 22.63 0L235.32 96a16 16 0 0 0 0-22.63ZM84.68 224L32 171.31l32-32l26.34 26.35a8 8 0 0 0 11.32-11.32L75.31 128L96 107.31l26.34 26.35a8 8 0 0 0 11.32-11.32L107.31 96L128 75.31l26.34 26.35a8 8 0 0 0 11.32-11.32L139.31 64l32-32L224 84.69Z"/></g></svg>
-            </label>
-            {/* Modal para guia de talles*/}
-            <input type="checkbox" id="my_modal_6" className="modal-toggle" />
-            <div className="modal" role="dialog">
-              <div className="modal-box bg-grey1 h-screen min-w-fit">
-                <h3 className="font-bold text-lg text-center pb-2">Guia de Talles</h3>
-                <img 
-                  className='rounded-lg'
-                  src='https://firebasestorage.googleapis.com/v0/b/imgs-tienda-renato.appspot.com/o/No%20articulos%2FGuia%20de%20Talles.jpg?alt=media&token=cd75d8df-a8a4-49dc-a423-4e97297f89a9' 
-                  alt='Guia de Talles'
-                />
-                <div className="modal-action flex">
-                  <label htmlFor="my_modal_6" className="btn">Cerrar</label>
-                </div>
-              </div>
-            </div>
             {terminar ? (
               <>
                 <Link to="/cart" className="static bottom-0 mb-0 btn bg-greyBtn text-white btn-block rounded">
